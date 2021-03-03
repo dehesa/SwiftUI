a07647768bba61231220cc171f1cbfa1e6864786
Marcos Sánchez-Dehesa Carballo <san.dehesa@gmail.com>
2021-03-03 18:27:38 +0100

    TabBar project added

---
+++ LBTA/TabBar/Sources/App.swift
@@ -0,0 +1,14 @@
+// - copyright: Lets Build That App ()
+// - seealso: https://www.letsbuildthatapp.com
+// - seealse: https://www.youtube.com/watch?v=9lVLFlyaiq4
+
+import SwiftUI
+
+/// Main entrace for the application.
+@main struct Application: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
